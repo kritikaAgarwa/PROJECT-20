e9e2947b96ed890e2fe1a895d9fc242c707bb2a6
kritikaAgarwa <71176275+kritikaAgarwa@users.noreply.github.com>
2020-12-17 12:08:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var car,wall;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+createSprite(400, 200, 50, 50);
+
+  car=createSprite(200,50,50,50);
+  wall=createSprite(1500,200,60,height/2);
+  speed=random(55,90);
+  weight=random(400,1500);
+  car.velocityX=speed;
+  wall.shapeColour(80,80,80); 
+
+  engine = Engine.create();
+  world = engine.world;
+	Engine.run(engine);
+  
+}
+
+function draw() {
+  background(255,255,255);  
+
+  if(wall.x-car.x<(car.width+wall.width)/2){
+    car.velocityX=0;
+    var deformation=0.5*weight*speed*speed/22500;
+    
+    if(deformation>180){
+      car.shapeColour=colour(255,0,0);
+    }
+    
+    if(deformation<180 && deformation>100){
+      car.shapeColour=colour(230,230,0);
+    }
+    
+    if(deformation<100){
+      car.shapeColour=colour(0,255,0);
+    }
+  }
+  drawSprites();
+}
